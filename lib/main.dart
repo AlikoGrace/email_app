ce07e3353c3a268dd9063405d92bf12427d58861
AlikoGrace <gracealiko09@gmail.com>
2023-03-12 07:23:40 +0000

    added the header section

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'app_styles.dart';
 import 'package:flutter/material.dart';
+import 'size_config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,19 +26,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return SingleChildScrollView(
       physics: BouncingScrollPhysics(),
       child: Column(
         children: [
-          Row(
-            children: [
-              Text('All Inboxes'),
-              CircleAvatar(
-                maxRadius: 26,
-                backgroundColor: kSecondaryColor,
-                foregroundImage: AssetImage('images/my_profile.jpg'),
-              )
-            ],
+          Container(
+            color: kBackgroundColor,
+            padding: EdgeInsets.only(top: 54, left: 24, right: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'All Inboxes',
+                          style: kJakartaHeading1.copyWith(
+                              fontSize: SizeConfig.blockScreenHorizontal! *
+                                  kHeading1),
+                        ),
+                        Icon(
+                          Icons.chevron_right,
+                          size: SizeConfig.blockScreenHorizontal! * kHeading3,
+                        )
+                      ],
+                    ),
+                    Text(
+                      'Total 2500 Messages, 3 Unread',
+                      style: kJakartaBodyMedium.copyWith(
+                          fontSize: SizeConfig.blockScreenHorizontal! * kBody2),
+                    ),
+                  ],
+                ),
+                CircleAvatar(
+                  maxRadius: 26,
+                  backgroundColor: kSecondaryColor,
+                  foregroundImage: AssetImage('images/my_profle.png'),
+                )
+              ],
+            ),
           )
         ],
       ),
